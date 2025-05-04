7bd0050169899241a86d9c8b420f59ab79947755
emranmuhammad <emran.a@xeleration.net>
2025-05-04 10:15:33 +0300

    Refactor AuthenticationLayout: implement login check and redirect to home if logged in

--- src/layouts/authentication/AuthenticationLayout.tsx
+++ src/layouts/authentication/AuthenticationLayout.tsx
@@ -1,4 +1,16 @@
+import { useSelector } from 'react-redux';
+import { Navigate, Outlet } from 'react-router-dom';
+import { RootState } from 'store';
+
 const AuthenticationLayout = () => {
-  return <div>AuthenticationLayout</div>;
+  const { loggedIn } = useSelector((state: RootState) => state.auth);
+  if (loggedIn) {
+    return <Navigate to={'/'} />;
+  }
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
 };
 export default AuthenticationLayout;
