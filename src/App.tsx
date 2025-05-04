ccd46fbaf178278755429961de77ac919bb59367
emranmuhammad <emran.a@xeleration.net>
2025-05-04 10:12:30 +0300

    Refactor App component: simplify imports, remove unused state and handlers, and streamline route rendering

--- src/App.tsx
+++ src/App.tsx
@@ -13,22 +13,18 @@ Coded by www.creative-tim.com
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
-import { useState, useEffect, useMemo, useCallback, ReactNode } from 'react';
+import { useEffect, useMemo } from 'react';
 
 // react-router components
-import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 // @mui material components
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import Icon from '@mui/material/Icon';
 
 // Material Dashboard 2 React components
-import MDBox from 'components/MDBox';
 
 // Material Dashboard 2 React example components
-import Sidenav from 'examples/Sidenav';
-import Configurator from 'examples/Configurator';
 
 // Material Dashboard 2 React themes
 import theme from 'assets/theme';
@@ -47,47 +43,25 @@ import themeDarkRTL from 'assets/theme-dark/theme-rtl';
 import routes from 'routes';
 
 // Material Dashboard 2 React contexts
-import {
-  useMaterialUIController,
-  setMiniSidenav,
-  setOpenConfigurator,
-  ControllerType,
-  DispatchFunction,
-} from 'context';
-import React from 'react';
+import { useMaterialUIController, ControllerType } from 'context';
 import rtlPlugin from 'stylis-plugin-rtl';
 
 // Images
-import brandWhite from 'assets/images/logo-ct.png';
-import brandDark from 'assets/images/logo-ct-dark.png';
 import createCache from '@emotion/cache';
 import { CacheProvider } from '@emotion/react';
-import { Box } from '@mui/material';
-import { AppRoute } from 'types';
 import GlobalSnackbar from 'components/GlobalSnackbar';
-import { useSelector } from 'react-redux';
-import { AppDispatch, RootState } from 'store';
+import { AppDispatch } from 'store';
 import { getUserData } from 'store/auth';
 import { useDispatch } from 'react-redux';
 import Loader from 'components/Loader';
-import { getEmployees } from 'store/employees';
+import { useAppSelector } from 'store/hooks';
 
 export default function App() {
-  const [controller, dispatch] = useMaterialUIController();
+  const [controller] = useMaterialUIController();
   const dispatchApp = useDispatch<AppDispatch>();
-  const {
-    miniSidenav,
-    direction,
-    layout,
-    openConfigurator,
-    sidenavColor,
-    transparentSidenav,
-    whiteSidenav,
-    darkMode,
-  } = controller as ControllerType;
-  const [onMouseEnter, setOnMouseEnter] = useState(false);
+  const { direction, darkMode } = controller as ControllerType;
   const { pathname } = useLocation();
-  const { loggedIn, isLoading } = useSelector((state: RootState) => state.auth);
+  const { isLoading } = useAppSelector((state) => state.auth);
 
   const cacheRtl = useMemo(
     () =>
@@ -99,26 +73,6 @@ export default function App() {
     // setRtlCache(cacheRtl)
     []
   );
-
-  // Open sidenav when mouse enter on mini sidenav
-  const handleOnMouseEnter = () => {
-    if (miniSidenav && !onMouseEnter) {
-      setMiniSidenav(dispatch as DispatchFunction, false);
-      setOnMouseEnter(true);
-    }
-  };
-
-  // Close sidenav when mouse leave mini sidenav
-  const handleOnMouseLeave = () => {
-    if (onMouseEnter) {
-      setMiniSidenav(dispatch as DispatchFunction, true);
-      setOnMouseEnter(false);
-    }
-  };
-
-  // Change the openConfigurator state
-  const handleConfiguratorOpen = () =>
-    setOpenConfigurator(dispatch as DispatchFunction, !openConfigurator);
 
   // Setting the dir attribute for the body element
   useEffect(() => {
@@ -133,120 +87,53 @@ export default function App() {
     }
   }, [pathname]);
 
-  const getRoutes = useCallback(
-    (allRoutes: AppRoute[]): ReactNode =>
-      allRoutes.map((route) => {
-        const redirectComponent = <Navigate to={'/'} />;
-        if (route.collapse) {
-          return getRoutes(route.collapse);
-        }
-        if (route.route) {
-          return (
-            <Route
-              path={route.route}
-              element={loggedIn === route.auth ? route.component : redirectComponent}
-              key={route.key}
-            />
-          );
-        }
-        return null;
-      }),
-    [loggedIn]
-  );
-
   useEffect(() => {
-    if (loggedIn) {
-      dispatchApp(getEmployees());
-    } else {
-      dispatchApp(getUserData());
-    }
-  }, [loggedIn]);
-
-  const configsButton = (
-    <MDBox
-      display='flex'
-      justifyContent='center'
-      alignItems='center'
-      width='3.25rem'
-      height='3.25rem'
-      bgColor='white'
-      shadow='sm'
-      borderRadius='50%'
-      position='fixed'
-      right='2rem'
-      bottom='2rem'
-      zIndex={99}
-      color='dark'
-      sx={{ cursor: 'pointer' }}
-      onClick={handleConfiguratorOpen}
-    >
-      <Icon fontSize='small' color='inherit'>
-        settings
-      </Icon>
-      <Box position={'fixed'} />
-    </MDBox>
-  );
-
-  if (isLoading) {
-    return <Loader />;
-  }
+    dispatchApp(getUserData());
+  }, []);
 
   return direction === 'rtl' ? (
     <CacheProvider value={cacheRtl}>
       <ThemeProvider theme={darkMode ? themeDarkRTL : themeRTL}>
         <GlobalSnackbar />
+        <Loader active={isLoading} />
         <CssBaseline />
-        {layout === 'dashboard' && (
-          <>
-            <Sidenav
-              color={sidenavColor}
-              brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
-              brandName='Material Dashboard 2'
-              routes={routes}
-              onMouseEnter={handleOnMouseEnter}
-              onMouseLeave={handleOnMouseLeave}
-            />
-            <Configurator />
-            {configsButton}
-          </>
-        )}
-        {layout === 'vr' && <Configurator />}
         <Routes>
-          <Route
-            path='/'
-            element={<Navigate to={`${loggedIn ? '/dashboard' : '/authentication/sign-in'}`} />}
-          />
-          {!isLoading && getRoutes(routes)}
-          <Route path='/load' element={<Loader />} />
+          {routes.map((route) => (
+            <Route element={route.component} path={route.route} key={route.key}>
+              {route.collapse?.map((nestedRoute) => (
+                <Route
+                  index={nestedRoute.index}
+                  key={nestedRoute.key}
+                  element={nestedRoute.component}
+                  path={!nestedRoute.index ? nestedRoute.route : undefined}
+                />
+              ))}
+            </Route>
+          ))}
+          <Route path='*' element={<h2 className='text-center'>not found</h2>} />
         </Routes>
       </ThemeProvider>
     </CacheProvider>
   ) : (
     <ThemeProvider theme={darkMode ? themeDark : theme}>
       <CssBaseline />
+      <Loader active={isLoading} />
       <GlobalSnackbar />
-      {layout === 'dashboard' && (
-        <>
-          <Sidenav
-            color={sidenavColor}
-            brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
-            brandName='Material Dashboard 2'
-            routes={routes}
-            onMouseEnter={handleOnMouseEnter}
-            onMouseLeave={handleOnMouseLeave}
-          />
-          <Configurator />
-          {configsButton}
-        </>
-      )}
-      {layout === 'vr' && <Configurator />}
       <Routes>
-        <Route
-          path='/'
-          element={<Navigate to={`${loggedIn ? '/dashboard' : '/authentication/sign-in'}`} />}
-        />
-        {!isLoading && getRoutes(routes)}
-        <Route path='/load' element={<Loader />} />
+        {routes.map((route) => (
+          <Route element={route.component} path={route.route} key={route.key}>
+            {route.collapse?.map((nestedRoute) => (
+              <Route
+                errorElement={<h2 className='text-center'>error</h2>}
+                index={nestedRoute.index}
+                key={nestedRoute.key}
+                element={nestedRoute.component}
+                path={!nestedRoute.index ? nestedRoute.route : undefined}
+              />
+            ))}
+          </Route>
+        ))}
+        <Route path='*' element={<h2 className='text-center'>not found</h2>} />
       </Routes>
     </ThemeProvider>
   );
