a5df6acf36acf5c8cac153e9fd12a7bdd7b99a89
emranmuhammad <emran.a@xeleration.net>
2025-05-04 10:13:43 +0300

    Refactor Loader component: add type definition for props and enhance styling for active state

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,8 +1,12 @@
+import { FC } from 'react';
 import './style.css';
 
-const Loader = () => {
+const Loader: FC<{ active?: boolean }> = ({ active }) => {
+  if (!active) {
+    return;
+  }
   return (
-    <div className='h-screen w-screen flex justify-center items-center'>
+    <div className='h-screen w-screen flex justify-center items-center absolute z-50 bg-white bg-opacity-25'>
       <div className='loader'></div>
     </div>
   );
